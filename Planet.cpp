309e9513656cd8855e16f1e14a433e3f0b6414a9
enkeif <enkeif@abv.bg>
2015-06-26 20:47:57 +0300

    Create Planet.cpp

---
+++ Planet.cpp
@@ -0,0 +1,24 @@
+#include "Planet.h"
+#icnlude<iostream>
+
+using namespace std;
+
+
+void Planet::destroy()
+{
+	population.erase(population.begin(), population.begin() + population.size()); //unishtojava cqloto naselenie na planetata
+}
+
+void Planet::add(Entity* e)
+{
+	population.push_back(e);
+}
+
+Planet::~Planet()
+{
+	while (!population.empty())
+	{
+		delete population.back();   //"delete" erases the memory allocated for the object of the vector
+		population.pop_back();
+	}
+}
