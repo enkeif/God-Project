4f522efecbd484495648a2648cde5048626518f4
enkeif <enkeif@abv.bg>
2015-06-26 20:24:35 +0300

    Create Human.cpp

---
+++ Human.cpp
@@ -0,0 +1,19 @@
+#include "Human.h"
+#include<iosteam>
+
+using namespace std;
+
+
+Human::Human() 
+{
+}
+
+Human::Human(string name,double energy,double power,double size,double weight,Point2D point,State state) : Animal(name, energy, power, size, weight, point, state);
+{                
+}                   
+
+void Human::Analyze(){
+    RandomG mind;
+    this->power = mind.RandomNumber();
+    this->setState(Analyzing);
+ }
