98d6295f0b7293f3240078b4b923deddcf779e1f
enkeif <enkeif@abv.bg>
2015-06-27 12:20:49 +0300

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -8,7 +8,7 @@ Human::Human()
 {
 }
 
-Human::Human(string name,double energy,double power,double size,double weight,Point2D point,State state) : Animal(name, energy, power, size, weight, point, state);
+Human::Human(string name,double energy,double power,double size,double weight,Point2D* point,State state) : Animal(name, energy, power, size, weight, point, state);
 {                
 }                   
 
